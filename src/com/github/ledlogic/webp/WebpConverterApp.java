187f0e8c4d85fde9739835b95312e8b92ee188d2
Jeff Conrad <ledlogic@gmail.com>
2026-01-12 03:14:46 -0600

    Change webp filenames

--- src/com/github/ledlogic/webp/WebpConverterApp.java
+++ src/com/github/ledlogic/webp/WebpConverterApp.java
@@ -5,12 +5,14 @@ import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class WebpConverterApp {
 	
 	private static WebpFilenameFilter filter = new WebpFilenameFilter();
 	
 	public static void main(String[] args) throws IOException, InterruptedException {
-		String attackFolder = "G:\\My Drive\\Games\\Traveller\\Traveller Scenarios\\Traveller CHEFs\\Agents";
+		String attackFolder = "G:\\My Drive\\Games\\Traveller\\Traveller Scenarios\\Traveller Solomani Rim\\05-Dingir Zone\\char-img";
 		File attackFolderFile = new File(attackFolder);
 		
 		File[] files = attackFolderFile.listFiles(filter);
@@ -18,11 +20,12 @@ public class WebpConverterApp {
 		String pattern = "yyyyMMddHHmm";
 		SimpleDateFormat simpleDateFormat = new SimpleDateFormat(pattern);
 		String date = simpleDateFormat.format(new Date());
+		boolean newNames = false;
 		
 		long cnt = 500;
 		for (File file: files) {
 			String inputFile = file.getName();
-			String outputFile = date + (cnt++) + ".png";			
+			String outputFile = (newNames ? date + (cnt++) : StringUtils.replace(file.getName(),".webp", "")) + ".png";			
 			
 			String inPath = attackFolder + "\\" + inputFile;
 			String outPath = attackFolder + "\\" + outputFile;
